cc9a40f3c25b897460a119d78f35a328da57a043
matias <pagosptcmatias@gmail.com>
2023-02-24 14:33:19 -0300

    cree la primer ventana y una sub ventana, ademas se modifico la barra de navegacion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const url = require("url");
+const path = require("path");
+const { BrowserRouter } = require("react-router-dom");
 
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+if (process.env.NODE_ENV !== "production") {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "../node_modules", ".bin", "electron"),
+  });
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+let mainWindow;
+let newProductWindow
 
-    <App />
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "views/Inicio.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
 
-);
+  const mainMenu = Menu.buildFromTemplate(templateMenu);
+  Menu.setApplicationMenu(mainMenu);
 
+});
+
+function createNewProuctWindow(){
+ newProductWindow = new BrowserWindow(
+    {
+      width: 400,
+      height: 330,
+      title:'agregar un nuevo producto a la ventana'
+    }
+  )
+  newProductWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "views/CardProducto.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
+}
+
+const templateMenu = [
+  {
+    label: "Archivo",
+    submenu: [
+      {
+        label: "Nuevo Producto",
+        accelerator: "Ctrl+N",
+        click() {
+          createNewProuctWindow()
+        },
+      },
+    ],
+  },
+];
